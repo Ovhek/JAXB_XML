8c30d522f8a3f67d6a17385479ebf0555dc125dd
Alexandru Ioan Cruceat - <cruceat.alexandru.ioan@alumnat.copernic.cat>
2023-01-27 19:54:42 +0000

    Infrastructura de los comandos

---
+++ src/main/java/Utils/CliManager.java
@@ -0,0 +1,105 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Utils;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+/**
+ * Clase Singleton encargada de manejar las opciones de la línea de comandos.
+ * @author Cole
+ */
+public class CliManager {
+    //Opciones de los comandos
+    private Options exportOptions;
+    private Options reportOptions;
+    private Options encryptOptions;
+    private Options decryptOptions;
+    private Options searchOptions;
+
+    public Options getExportOptions() {
+        return exportOptions;
+    }
+
+    public Options getReportOptions() {
+        return reportOptions;
+    }
+
+    public Options getEncryptOptions() {
+        return encryptOptions;
+    }
+
+    public Options getDecryptOptions() {
+        return decryptOptions;
+    }
+
+    public Options getSearchOptions() {
+        return searchOptions;
+    }
+
+    public CommandLine getCmd() {
+        return cmd;
+    }
+    
+    //Línea de comandos con las opciones
+    private CommandLine cmd;
+    
+    /**
+     * Constructor que instancia las opciones de los comandos.
+     */
+    public CliManager() {
+        cmd = null;
+        addAllOptions();
+    }
+    
+    /**
+     * Función encargada de asginar las opciones de los comandos.
+     */
+    private void addAllOptions() {
+        exportOptions = new Options();        
+        //Exportar
+        exportOptions.addOption("f", "format", true, "Formato de salida (CSV, XML o CSVXML)");
+        exportOptions.addOption("o", "output", true, "Directorio del archivo de salida");
+        exportOptions.addOption("c", "columna", true, "Elemento sobre el cual ordenar la exportación");
+        
+        // Reporte
+        reportOptions = new Options();  
+        reportOptions.addOption("opcion_exportar", true, "Opción de exportación");
+        reportOptions.addOption("directorio", true, "Directorio de Exportación");
+        reportOptions.addOption("tipo_informe", true, "Tipo de informe");
+        
+        //Encriptar
+        encryptOptions = new Options();
+        encryptOptions.addOption("i", "input", true, "Directorio del archivo de entrada");
+        encryptOptions.addOption("o", "output", true, "Directorio del archivo de salida");
+        encryptOptions.addOption("k", "key", true, "Clave de Encriptación/Desencriptación");
+        
+        //Desencriptar
+        decryptOptions = new Options();
+        decryptOptions.addOption("i", "input", true, "Directorio del archivo de entrada");
+        decryptOptions.addOption("o", "output", true, "Directorio del archivo de salida");
+        decryptOptions.addOption("k", "key", true, "Clave de Encriptación/Desencriptación");
+        
+        // Buscar
+        searchOptions = new Options();
+        searchOptions.addOption("elemento", true, "Elemento a buscar");
+        searchOptions.addOption("nombre", true, "Nombre del elemento a buscar");
+    }
+    
+    /**
+     * Función encargada de parsear la linea de comandos con las opciones dadas
+     * @param options Las opciones que están disponibles
+     * @param args Argumentos de la linea de comandos
+     * @return Linea de comandos parseada
+     */
+    public CommandLine parseOptions(Options options,String[] args) throws ParseException {
+        CliManager manager = new CliManager();
+        cmd = manager.parseOptions(options, args);
+        return cmd;
+    }
+}
---
+++ src/main/java/Utils/ErrorMessagesHelpers.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Utils;
+
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+
+/**
+ * Clase que muestra los mensajes de ayuda de los comandos
+ */
+public abstract class ErrorMessagesHelpers {
+
+    /**
+     * Función encargada de mostrar el mensaje de ayuda de un comando.
+     * @param options Opciones del comando
+     * @param command Comando en cuestión
+     */
+    public static void printCommandHelp(Options options,String command, Exception e) {
+        HelpFormatter formatter = new HelpFormatter();
+        System.out.println("El comando se ha utilizado de forma incorrecta.");
+        System.out.println(e.getMessage());
+        formatter.printHelp(command, options);
+        System.exit(0);
+    }
+    /**
+     * Función encargada de mostrar la ayuda de los comandos principales.
+     */
+    public static void printMainHelper() {
+        System.out.println("Comandos soportados:");
+        System.out.println("\treport\t --- Creación de reportes.");
+        System.out.println("\texport\t --- Exportar el archivo.");
+        System.out.println("\tencrypt\t --- Encrypta un archivo.");
+        System.out.println("\tdecrypt\t --- Desencripta un archivo.");
+        System.out.println("\tsearch\t --- Busca en el archivo");
+        System.exit(0);
+    }
+}
