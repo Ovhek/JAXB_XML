33d25cd32b4ceaeba48e607dda49cd4cf6d0e86f
Alexandru <alexandrucruceat@gmail.com>
2023-03-24 10:52:11 +0100

    Infraestructura Principal

---
+++ src/main/java/m06uf3pracma/dao/RepositorioDAO.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package m06uf3pracma.dao;
+
+import m06uf3pracma.model.Repositorio;
+
+/**
+ *
+ * @author Alex
+ */
+public class RepositorioDAO extends GenericDAO<Repositorio>{
+    
+    public RepositorioDAO(Class<Repositorio> clazz) {
+        super(clazz);
+    }
+    
+}
